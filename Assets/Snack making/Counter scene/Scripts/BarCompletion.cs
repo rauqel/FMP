6e5832951b58e703276bf788b403da6d6ff4f6ff
rauqel <ralfonso2021@elam.co.uk>
2022-05-19 12:41:06 +0100

    working completion bar for snack making activity

--- Assets/Snack making/Counter scene/Scripts/BarCompletion.cs
+++ Assets/Snack making/Counter scene/Scripts/BarCompletion.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class BarCompletion : MonoBehaviour
 {
-    Image completionBar;
+    public Image completionBar;
 
     public Bread2UIscript bread2Check;
     public BreadUIscript breadCheck;
@@ -13,56 +13,124 @@ public class BarCompletion : MonoBehaviour
     public SprinkleUIscript sprinkleCheck;
 
     float progress = 0f;
-    float currentProgress1 = 0f;
-    float currentProgress2 = 0f;
-    bool progressAdded1;
+
+    bool counterAdding1 = true;
+    bool counterAdding2 = true;
+    bool counterAdding3 = true;
+    bool counterAdding4 = true;
+    //
     bool counterAdded1;
+    bool counterAdded2;
+    bool counterAdded3;
+    bool counterAdded4;
     int counter;
 
     void Start()
     {
-        completionBar = GetComponent<Image>();
+        completionBar = completionBar.GetComponent<Image>();
     }
 
     void Update()
     {
+        completionBar.fillAmount = Mathf.Lerp(0f, 1.0f, progress);
+        //
         if (breadCheck.breadSlice.activeSelf)
         {
-            if (!counterAdded1)
+            if (counterAdding1)
             {
                 counter += 1;
-                counterAdded1 = true;
+                counterAdding1 = false;
             }
         }
         if (knifeCheck.knifeButtered)
         {
-            counter += 1;
+            if (counterAdding2)
+            {
+                counter += 1;
+                counterAdding2 = false;
+            }
         }
         if (bread2Check.butterOn)
         {
-            counter += 1;
+            if (counterAdding3)
+            {
+                counter += 1;
+                counterAdding3 = false;
+            }
         }
         if (bread2Check.sprinklesOn)
         {
-            counter += 1;
-        }
-        Debug.Log(counter);
-        completionBar.fillAmount = Mathf.Lerp(0f, 1.0f, progress);
-
-        if (counter == 1)
-        {
-            if (progress < currentProgress1 + 0.25f)
+            if (counterAdding4)
             {
-                progress += Time.deltaTime / 3.5f;
-                //progressAdded1 = true;
+                counter += 1;
+                counterAdding4 = false;
             }
         }
-        if (knifeCheck.knifeButtered)
+        Debug.Log(counter);
+        //
+        //
+        //
+        if (counter == 1)
         {
-            if (progress < currentProgress2 + 0.25f)
+            counterAdded1 = true;
+        }
+        if (counter == 2)
+        {
+            counterAdded2 = true;
+        }
+        if (counter == 3)
+        {
+            counterAdded3 = true;
+        }
+        if (counter == 4)
+        {
+            counterAdded4 = true;
+        }
+        //
+        //
+        //
+        if (counterAdded1)
+        {
+            if (progress < 0.25)
             {
-                progress += Time.deltaTime / 3.5f;
-                //progressAdded2 = true;
+                progress += Time.deltaTime / 1.5f;
+            }
+            else
+            {
+                counterAdded1 = false;
+            }
+        }
+        if (counterAdded2)
+        {
+            if (progress < 0.5)
+            {
+                progress += Time.deltaTime / 1.5f;
+            }
+            else
+            {
+                counterAdded2 = false;
+            }
+        }
+        if (counterAdded3)
+        {
+            if (progress < 0.75)
+            {
+                progress += Time.deltaTime / 1.5f;
+            }
+            else
+            {
+                counterAdded3 = false;
+            }
+        }
+        if (counterAdded4)
+        {
+            if (progress < 1)
+            {
+                progress += Time.deltaTime / 1.5f;
+            }
+            else
+            {
+                counterAdded4 = false;
             }
         }
     }
